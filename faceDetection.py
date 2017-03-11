4ff9fa0266d2083b897e40e2f628a5fb399dc31b
JeeShao <jee_shao@163.com>
2017-03-11 15:10:20 +0800

    harrcascades SetectMultiScale检测

--- faceDetection.py
+++ faceDetection.py
@@ -7,11 +7,11 @@ def detect(filename):
     # print img
     # exit()
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray,1.3,5)
+    faces = face_cascade.detectMultiScale(gray,1.1,5)
     for(x,y,w,h) in faces:
         img = cv2.rectangle(img,(x,y),(x+w,y+w),(255,0,0),2)
     cv2.namedWindow('MyFace',0)
     cv2.imshow('MyFace',img)
-    cv2.imwrite('./MyFace.jpg',img)
+    cv2.imwrite('./MyFace1.jpg',img)
     cv2.waitKey(0)
 detect(filename)
