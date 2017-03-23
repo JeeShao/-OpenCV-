c4e36d37d39971e68b828fff7d40abc2b3473c9d
JeeShao <jee_shao@163.com>
2017-03-24 02:11:43 +0800

    equalizeHist

--- videoFaceDetection.py
+++ videoFaceDetection.py
@@ -50,17 +50,27 @@ def face_rec():
     face_cascade = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_alt.xml')
     while(True):
         read,img = camera.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  #灰度化
+        #分离彩色图三通道
+        # b, g, r = cv2.split(frame)
+        # # cv2.imshow("Blue", r)
+        # 直方图均衡化
+        # hist_b=cv2.equalizeHist(b)
+        # hist_g=cv2.equalizeHist(g)
+        # hist_r=cv2.equalizeHist(r)
+        #合并三通道(顺序是bgr)
+        # img = cv2.merge(([hist_b, hist_g, hist_r]))
         # img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(img, 1.3, 5)
-        print(faces)
-        # exit()
-        for (x, y, w, h) in faces:
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        print("faces",faces)
+        for (x, y, w, h) in faces: #多张人脸时 循环识别
             img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-            roi = gray[x:x+w,y:y+h]
+            # gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            roi = gray[x:x+w,y:y+h] #灰度人脸图
             try:
-                roi = cv2.resize(roi, (92, 112),interpolation=cv2.INTER_LINEAR)
-                params = model.predict(roi)
+                roi = cv2.resize(roi, (92, 112),interpolation=cv2.INTER_LINEAR)#格式化
+                hist_roi = cv2.equalizeHist(roi)  # 均衡直方图
+                params = model.predict(hist_roi)
                 print("Lable: %s, Confidence: %.2f" % (params[0],params[1]))
                 p=list(params)
                 if p[0]==0:
